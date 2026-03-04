140d3c0182602242a105b6a9aba03fa47f16fa8f
ianmllr <hrjensen243@gmail.com>
2026-03-04 21:38:07 +0100

    vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,7 @@ export default function Home() {
                         ))}
                     </div>
                 </div>
+                <Analytics />
             </main>
         </>
     )
