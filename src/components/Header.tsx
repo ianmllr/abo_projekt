014976297a56231434147656eed5887c1e233c02
ianmllr <hrjensen243@gmail.com>
2026-03-04 23:05:17 +0100

    change in header and update order of scrapers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,16 @@ export default function Header() {
             <div className="max-w-[1200px] mx-auto flex items-center justify-between">
                 <div className="flex items-center gap-2">
                     <Image src="/favicon.ico" alt="Logo" width={32} height={32} />
-                    <Link href="/" >
-                    <span className="text-[#cdd6e0] text-xl font-semibold tracking-tight">Tech-tilbud</span>
+                    <Link href="/">
+                        <span className="text-[#cdd6e0] text-xl font-semibold tracking-tight">Tech-tilbud</span>
                     </Link>
                 </div>
-                <nav className="text-[#7d8fa0] text-sm ">
+                <nav className="flex items-center gap-3 text-[#7d8fa0] text-sm">
                     <Link href="/about" className="px-3 py-1.5 rounded-md border border-[#334155] hover:border-[#4a90b8] hover:text-[#cdd6e0] transition-colors">
-                        Om Tech-tilbud
+                        Sæt en påmindelse
+                    </Link>
+                    <Link href="/about" className="px-3 py-1.5 rounded-md border border-[#334155] hover:border-[#4a90b8] hover:text-[#FFFFFF] transition-colors">
+                        Hvad er Tech-tilbud?
                     </Link>
                 </nav>
             </div>
